85c5a9cb92f06fe1470895db5a49eea290eed3c5
Muhammed Umer S <mohdumer77md@gmail.com>
2025-10-26 14:08:06 +0530

    feat: Initial setup for Spring Boot backend and React frontend

---
+++ Backend/src/main/java/com/resource_booking/CollegeResourceBookingApplication.java
@@ -0,0 +1,13 @@
+package com.resource_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeResourceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeResourceBookingApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/resource_booking/CollegeResourceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeResourceBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
